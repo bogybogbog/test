48aaf1b633d972f31874666b635b7d60f6bdf82c
ahmed mahmoud <ahmad.mahmoud1472003@gmail.com>
2024-09-25 22:48:18 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+"use strict"
+
+
+// let x;
+// console.log(x);
+// console.log(y);
+// let nums = [10,20,30]
+// for(var x of nums){
+//     console.log(x);
+// }
+ 
+// ! ternary operator :
+
+let age = 9;
+age < 18 ? console.log("not allowed") : console.log("welcome");
+;
