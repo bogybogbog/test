e0d6fcde85f447b93d6f50db149637593ff7adcb
ahmed mahmoud <ahmad.mahmoud1472003@gmail.com>
2024-09-27 15:22:08 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -50,23 +50,51 @@
 
 // ! destructing
 // ? OBJECTS
-let user = {
-  userName: "ahmed",
-  age: 21,
-  salay: 2200,
-  son: { fname: "bog", age1: 4 },
-};
+// let user = {
+//   userName: "ahmed",
+//   age: 21,
+//   salay: 2200,
+//   son: { fname: "bog", age1: 4 },
+// };
 
-let { userName, age, salay } = user; // this means ===> let userName = user.userName  ..... and it has to be the same name as the propirity
-let { age1, fname } = user.son;
-console.log(age1);
+// let { userName, age, salay } = user; // this means ===> let userName = user.userName  ..... and it has to be the same name as the propirity
+// let { age1, fname } = user.son;
+// console.log(age1);
 
 // ? ARRAYS
 
-let friends = ["ahmed", "mahmoud", "saeed"];
+// let friends = ["ahmed", "mahmoud", "saeed"];
 
-let [x, y, z] = friends
-console.log(x);
+// let [x, y, z] = friends
+// console.log(x);
 
-// ! shallow copy and deep copy and map 
+// ! shallow copy and deep copy, map and set
 
+// ! this
+// console.log(this); // window
+
+// document.addEventListener("click", function () {
+//   console.log(this);
+// });
+
+let user = {
+  age: 21,
+  name: "ahmed",
+  salay: 2000,
+  calcSalary: function () {
+    // let that = this   // we did this cuz "this" is only defined in calcSalary() scope
+    let calcTax = () => {
+      // return (that.salay * 10) / 100;
+      return (this.salay * 10) / 100;
+    };
+    return this.salay - calcTax();
+  },
+};
+console.log(user.calcSalary());
+
+// ! Arrow function
+
+// let test = () => "test"; // return one line ===> we can remove "return" and "{}"
+// let test = _ => "test"; // with no prameter ===> we can remove "()"
+let test = (name) => "test " + name; // with one prameter ===> we can remove "()" and put the prameter
+console.log(test("aaaaah"));
